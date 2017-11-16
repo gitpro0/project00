e38df73b4792fa03bd9381f76423b479514facb8
buildup-code <33623846+buildup-code@users.noreply.github.com>
2017-11-16 17:16:59 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
+
+// hey man i made test 
 int main()
 {
-
+  int x;
+  printf("what we can do as first project\n");
+  printf("1- c project\n 2- arduino code\nCHOOSE THE NUMBER : ");
+  scanf('%d',&x);
+  if(x!=1|2) do printf("please follow the instruction")else
+  { if(x=1) printf("wrong");
+   if(x=2) printf("GO AHEAD");
+  }
 }
